dec25e47e6b465597ca1c32c5fc750f4391a5ae2
bezsozs <bezsozs@gmail.com>
2018-05-23 20:42:00 +0100

    initial commit

---
+++ hello/hello.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+from flask import Flask
+
+app= Flask(__name__)
+
+@app.route("/")
+
+def index():
+    return "Hello Word"
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
+
+
+
+
+
+
+
+
